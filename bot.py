3f7fef5c738774ea1f9b8ffb2e0b8427fc245b58
Yokasashii <vincentguerrini07@gmail.com>
2026-03-11 14:15:07 +0100

    limit embed fields

--- bot.py
+++ bot.py
@@ -245,6 +245,8 @@ async def team_classement(interaction: discord.Interaction):
                 value=f"{team.points} points",
                 inline=False,
             )
+            if index >= 24:
+                break
 
         if i == 0:
             await interaction.response.send_message(embed=embed)
